0552a775fd18f6ac6e4d3c74934af514b0e37d29
Raúl Eduardo Durán Carrasco <raul.d.carrasco@outlook.com>
2016-03-13 17:26:23 +0000

    Login and Logout functions added.

--- data/users.js
+++ data/users.js
@@ -28,3 +28,21 @@ exports.signup = function(req, res) {
         }
     })
 }
+
+exports.login = function(req, res) {
+    users.findOne({ username: req.body.userName }, function(err, data) {
+        if(err) throw err
+        if(data != null) {
+            req.session.userName = req.body.userName
+            res.status(200).send({ connected: true })
+        }
+        else {
+            res.status(403).send({ connected: false })
+        }
+    })
+}
+
+exports.logout = function(req, res) {
+    req.session.userName = null
+    res.status(200).send({ disconnected: false })
+}
